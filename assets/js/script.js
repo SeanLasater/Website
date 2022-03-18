5b3379777b3ec145748cf2f6915597bd9ca14661
Sean Lasater <seanlasater@gmail.com>
2022-03-18 14:41:22 -0700

    class/id names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-var inputName = document.getElementById('nameText').innerHTML;
-var birthDate = document.getElementById('birthday').innerHTML;
-const submitButton = document.getElementById('astroButton').innerHTML;
+var inputName = document.getElementById('nameField').innerHTML;
+var birthDate = document.getElementById('birthdayField').innerHTML;
+const submitButton = document.getElementsByClassName('submit').innerHTML;
 
 function submit() {
     window.alert('This site lacks functionality at this time.');
